1147e02f86a685adbd563e5cd860dac75a193980
mstiles92 <mstiles92@gmail.com>
2016-06-19 03:42:24 -0400

    Set up companion object with plugin instance

--- src/main/kotlin/com/mstiles92/plugins/keepchestnbt/KeepChestNBT.kt
+++ src/main/kotlin/com/mstiles92/plugins/keepchestnbt/KeepChestNBT.kt
@@ -3,5 +3,12 @@ package com.mstiles92.plugins.keepchestnbt
 import org.bukkit.plugin.java.JavaPlugin
 
 class KeepChestNBT : JavaPlugin() {
+    companion object {
+        var instance: KeepChestNBT? = null
+        private set
+    }
 
+    override fun onEnable() {
+        instance = this
+    }
 }
