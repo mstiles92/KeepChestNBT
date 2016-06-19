0465ed7a0bb4831c3c4e2eb9363235c6f3458a6f
mstiles92 <mstiles92@gmail.com>
2016-06-19 04:04:34 -0400

    Add PluginMetrics to send anonymous usage statistics to MCStats

--- README.md
+++ README.md
@@ -3,3 +3,8 @@
 This plugin allows non-op players to place chests with inventory contents stored in NBT, overriding the default behavior of wiping out the contents of the inventory.
 
 No commands, no permissions, just drop it in to the plugins directory to gain this functionality.
+
+
+## Stats Collection ##
+
+Anonymous statistics are collected automatically by this plugin and set to MCStats. If you would like to view the data, you can do so [here](http://mcstats.org/plugin/KeepChestNBT). If you would like to disable this feature, simply set "opt-out" to true in plugins/PluginMetrics/config.yml.
--- src/main/kotlin/com/mstiles92/plugins/keepchestnbt/KeepChestNBT.kt
+++ src/main/kotlin/com/mstiles92/plugins/keepchestnbt/KeepChestNBT.kt
@@ -2,6 +2,8 @@ package com.mstiles92.plugins.keepchestnbt
 
 import com.mstiles92.plugins.keepchestnbt.listeners.BlockListener
 import org.bukkit.plugin.java.JavaPlugin
+import org.mcstats.Metrics
+import java.io.IOException
 
 class KeepChestNBT : JavaPlugin() {
     companion object {
@@ -13,5 +15,12 @@ class KeepChestNBT : JavaPlugin() {
         instance = this
 
         server.pluginManager.registerEvents(BlockListener, this)
+
+        try {
+            val metrics = Metrics(this)
+            metrics.start()
+        } catch (e: IOException) {
+            logger.warning("Unable to start Plugin Metrics!")
+        }
     }
 }
